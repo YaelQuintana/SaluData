9f6c198a89eadcab6424909be3d7f3784732c64b
YaelQuintana <l20212116@tectijuana.edu.mx>
2024-03-16 17:36:56 -0700

    after bind|menu working

--- app/src/main/java/com/example/saludata/MainActivity.kt
+++ app/src/main/java/com/example/saludata/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.saludata
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.saludata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root) //R.layout.activity_main
+        replaceFragment(Home())
+
+        binding.buttonNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.Home -> replaceFragment(Home())
+                R.id.profile -> replaceFragment(Profile())
+                R.id.calendar -> replaceFragment(Calendar())
+
+                else ->{
+
+                }
+
+
+            }
+            true
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
