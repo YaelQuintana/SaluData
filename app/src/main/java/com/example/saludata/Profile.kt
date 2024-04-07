1319e9fe5d9f055e765e8fc407b13fb256c4f338
Yael Quintana <l20212116@tectijuana.edu.mx>
2024-04-06 21:34:29 -0700

    icons

--- app/src/main/java/com/example/saludata/Profile.kt
+++ app/src/main/java/com/example/saludata/Profile.kt
@@ -11,11 +11,7 @@ import android.widget.ImageView
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Profile.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class Profile : Fragment() {
     // TODO: Rename and change types of parameters
     private lateinit var imageView2: ImageView
