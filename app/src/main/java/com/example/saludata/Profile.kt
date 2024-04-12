29460fb45364fd5c80f462ccaff31412ebec599d
YaelQuintana <l20212116@tectijuana.edu.mx>
2024-04-12 15:24:58 -0700

    minor stuff

--- app/src/main/java/com/example/saludata/Profile.kt
+++ app/src/main/java/com/example/saludata/Profile.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 
 // TODO: Rename parameter arguments, choose names that match
@@ -15,6 +16,9 @@ import android.widget.ImageView
 class Profile : Fragment() {
     // TODO: Rename and change types of parameters
     private lateinit var imageView2: ImageView
+
+    //bottom sheet
+    private lateinit var button_Bienvenidos: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,10 @@ class Profile : Fragment() {
        // return inflater.inflate(R.layout.fragment_profile, container, false)
 
         return view
+    }
+
+    override fun showBottomSheet() {
+
     }
 
 
