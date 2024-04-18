f304e67f27f693560f4031d8971e69138b8b4dad
Yael Quintana <l20212116@tectijuana.edu.mx>
2024-04-18 01:37:36 -0700

    google json | firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    //google firebase
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -53,4 +55,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Database
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
 }
